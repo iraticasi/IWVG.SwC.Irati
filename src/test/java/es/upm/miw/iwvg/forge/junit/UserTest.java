f6c94b6548c1d24f37058a99ef717f5a307943e4
iraticasi <iraticasi@gmail.com>
2018-10-02 20:10:59 +0200

    tests para nuevas funcionalidades

---
+++ src/test/java/es/upm/miw/iwvg/forge/junit/UserTest.java
@@ -0,0 +1,33 @@
+package es.upm.miw.iwvg.forge.junit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void before() {
+        user = new User(1, "Irati", "Casi");
+    }
+
+    @Test
+    void testFullName() {
+        assertEquals("Irati Casi", user.fullName());
+    }
+
+    @Test
+    void testInitials() {
+        assertEquals("I.", user.initials());
+    }
+
+    @Test
+    void testUppercaseName(){
+        assertEquals("IRATI", user.uppercaseName());
+    }
+
+}
